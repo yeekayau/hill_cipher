c6a3b323c96263a15631606e1d5938d4c57e3a0a
yyau0774 <yyau0774@yyau0774s-mbp.staff.wireless.sydney.edu.au>
2024-02-26 15:29:43 +1100

    Show matrix

--- hill_cipher.py
+++ hill_cipher.py
@@ -2,4 +2,15 @@ import streamlit as st
 import random
 import string
 
+import hill_cipher_functions as hcf
+
 st.title("Hill Cipher")
+
+plain_text = st.text_area("Enter plain text for encryption")
+
+n = st.text_input("Enter the dimensions of the matrix")
+
+st.markdown("Here is a random inveritible matrix key: ")
+
+A = ncf.random_invertible_matrix(n)
+A
---
+++ hill_cipher_functions.py
@@ -0,0 +1,114 @@
+import numpy as np
+from numpy.linalg import inv
+
+def random_invertible_matrix(n):
+  """
+  Generates a random n by n invertible integer valued matrix.
+
+  Args:
+      n: The size of the matrix.
+
+  Returns:
+      A random n by n invertible integer valued matrix.
+  """
+
+  # Generate a random integer matrix.
+  matrix = np.random.randint(0, 200, size=(n, n))
+
+  # Check if the matrix is invertible.
+  while not np.linalg.det(matrix):
+    matrix = np.random.randint(0, 200, size=(n, n))
+
+  # Return the invertible matrix.
+  return matrix
+
+
+def plain_text_prep(text):
+    """
+    This function takes a string as input, converts to uppercase and removes all punctuation marks and spaces from it.
+
+    Args:
+    - text (str): The input string to remove punctuation from.
+
+    Returns:
+    - str: The input string without any punctuation marks.
+    """
+    text = text.upper()
+
+    # Only include letters
+    text_no_punct = ''.join(char for char in text if char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+    text_no_punct.replace('“', "") # remove double quotation marks
+
+    return text_no_punct.upper().replace(" ", "")
+
+def partition_and_numberfy(cleaned_text, n):
+  """
+    Partition a cleaned piece of text into blocks of size n, and return the set of vectors of encoded text. 
+    If n does not divide the length of the text, a few random letters are added, so that the length of the text is a multiple of n.
+
+    Args:
+      cleaned_text: uppercase string of letters with no spacing.
+      n: dimensions of square matrix
+
+    Returns:
+      set of vectors encoding the text.
+  """
+
+  uppercase_alphabet = string.ascii_uppercase
+  mappings = list(zip(uppercase_alphabet, [i for i in range(0,26)]))
+
+  # returns list of letters in the text as their number counterparts
+  numberfied = [letter[1] for i in cleaned_text for letter in mappings if i == letter[0] ]
+
+  # Case when n (dimensions of the matrix) does not divide the length of the text
+  if len(numberfied)%n != 0:
+    num_randoms_needed = (len(cleaned_text)//n + 1)*n - len(numberfied)
+
+    for i in range(num_randoms_needed):
+      numberfied.append(random.randint(0,25)) # add some random letters
+
+  return [ numberfied[i:i+n] for i in [k*n for k in range(0, len(cleaned_text)//n + 1)] if i != len(numberfied) ]
+
+
+
+def encrypt_blocks(input_text, A):
+  """
+    Encrypts the input text using the Hill cipher with square invertible matrix A.
+    Returns the encrypted text as an np array of blocks modulo 25 (i.e. the encrypted text)
+
+    Args:
+      input_text:  a string.
+      A: A square invertible matrix
+
+    Returns:
+      Hill cipher encrypted text
+  """
+
+  clean_text = plain_text_prep(input_text)
+
+  encoded_blocks = partition_and_numberfy(clean_text, A.shape[0])
+
+  # encrypted matrix - not modded.
+  encrypted = [np.dot(A,np.array(r)) for r in encoded_blocks]
+  
+  # Reduce entries mod 25
+  encrypted_mod25 = [r%25 for r in encrypted]
+
+  return encrypted_mod25
+
+
+def encrypt_hill(input_text, A):
+  '''
+  Encrypt hill cipher
+  '''
+  encrypted_blocks = encrypt_blocks(input_text, A)
+  
+  uppercase_alphabet = string.ascii_uppercase
+  mappings = list(zip(uppercase_alphabet, [i for i in range(0,26)]))
+
+  cipher_text_list = [k[0] for r in encrypted_blocks for l in r for k in mappings if l == k[1]]
+
+  cipher_text = ''.join([i for i in cipher_text_list])
+
+  return cipher_text
+
