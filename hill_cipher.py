6eec75f63378116172fe17634ef190e03a942858
yyau0774 <yyau0774@192-168-1-139.tpgi.com.au>
2024-03-17 10:49:03 +1100

    clean up display

--- hill_cipher.py
+++ hill_cipher.py
@@ -28,8 +28,9 @@ if plain_text and n:
 	letters_and_numbers = zip([i for i in cleaned_text], [i for k in encoded_blocks for i in k])
 
 	st.write("Plain text and encoded text (mod 26:")
-		for item in letters_and_numbers:
-			st.write(item[0], end=' ')  # Print the first entry of the tuple
-		for item in letters_and_numbers:  # Re-zip since it was consumed in the previous loop
-			st.write(item[1], end=' ')  # Print the second entry of the tuple
+	
+	for item in letters_and_numbers:
+		st.write(item[0], end=' ')  # Print the first entry of the tuple
+	for item in letters_and_numbers:  # Re-zip since it was consumed in the previous loop
+		st.write(item[1], end=' ')  # Print the second entry of the tuple
 
